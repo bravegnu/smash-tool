a00d185199ae7bc3a06bf8af6b9e876707bcefdb
Govintharaj <53900377+Govintharaj@users.noreply.github.com>
2019-12-11 15:54:56 +0530

    added testcases for hex and micro (#8)
    Added testcases for hex and micro.

---
+++ smashlib/test_hex.py
@@ -0,0 +1,136 @@
+import io
+import unittest
+from unittest.mock import Mock, patch
+from .hexfile import Hex, HexFile, HexError
+
+class HexTest(unittest.TestCase):
+
+    def setUp(self):
+        self.hex_data = Hex(b":03000000020008")
+        hex_file = io.BytesIO(b":03000000020008f3")
+        self.m_open = Mock(return_value=hex_file)
+
+    def test_checksum(self):
+        expected_value = b":03000000020008f3"
+        result = self.hex_data.append_checksum(b":03000000020008")
+        self.assertEqual(result, expected_value)
+
+    def test_get_type(self):
+        self.assertEqual(self.hex_data.get_type(), b"00")
+
+    def test_is_data(self):
+        self.assertEqual(self.hex_data.is_data(), 1)
+
+    def test_is_eof(self):
+        self.assertEqual(self.hex_data.is_eof(), 0)
+
+    def test_addr(self):
+        self.assertEqual(self.hex_data.addr(), 0000)
+
+    def test_data(self):
+        expected_value = (2, 0, 8)
+        result = self.hex_data.data()
+        self.assertEqual(result, expected_value)
+
+    def test_datalen(self):
+        self.assertEqual(self.hex_data.datalen(), 3)
+
+    def test_get_hex(self):
+        expected_value = b":03000000020008"
+        self.assertEqual(self.hex_data.get_hex(), expected_value)
+
+    def test__bytes__(self):
+        expected_value = b":03000000020008"
+        self.assertEqual(self.hex_data.__bytes__(), expected_value)
+
+    def test_block_from_addr(self):
+        expected_value = 0
+        addr = 1010
+        addr_range = [(0000, 1111)]
+        with patch('builtins.open', self.m_open):
+            hex_read = HexFile('')
+        result = hex_read.block_from_addr(addr, addr_range)
+        self.assertEqual(result, expected_value)
+
+    def test_iter(self):
+        with patch('builtins.open', self.m_open):
+            hex_read = HexFile('')
+        self.assertEqual(hex_read.rewind(), None)
+
+    def test_data_bytes(self):
+        with patch('builtins.open', self.m_open):
+            hex_read = HexFile('')
+        self.assertEqual(hex_read.count_data_bytes(), 3)
+
+    def test_data_data(self):
+        expected_value = [(0, 2), (1, 0), (2, 8)]
+        with patch('builtins.open', self.m_open):
+            hex_read = HexFile('')
+        data = hex_read.data_bytes()
+        self.assertEqual(list(data), expected_value)
+
+    def test_used_blocks(self):
+        addr_range = [(0000, 1111)]
+        with patch('builtins.open', self.m_open):
+            hex_read = HexFile('')
+        result = hex_read.used_blocks(addr_range)
+        self.assertEqual(result, [0])
+
+    def test_hex_error(self):
+        result = self.hex_data.append_checksum
+        self.assertRaises(HexError, result, 'N/A')
+
+    def test_hexfile_error(self):
+        self.assertRaises(HexError, Hex(":dfgh").data)
+
+    def test_addr_error(self):
+        self.assertRaises(HexError, Hex(":dfgh").addr)
+
+    def test_datalen_error(self):
+        self.assertRaises(HexError, Hex(":ggg").datalen)
+
+class HexfileTest(unittest.TestCase):
+
+    def test_count_error(self):
+        hex_file = io.BytesIO(b":03000000")
+        m_open = Mock(return_value=hex_file)
+        hex_file.name = ''
+        with patch('builtins.open', m_open):
+            hex_read = HexFile('')
+        self.assertRaises(HexError, hex_read.count_data_bytes)
+
+    def test_file_error(self):
+        hex_file = io.BytesIO(b":03000001020008F3")
+        m_open = Mock(return_value=hex_file)
+        with patch('builtins.open', m_open):
+            hex_read = HexFile('')
+        self.assertRaises(StopIteration, next, hex_read)
+
+    def test_block_error(self):
+        hex_file = io.BytesIO(b":03gggg00020008F3")
+        m_open = Mock(return_value=hex_file)
+        hex_file.name = ''
+        expected_value = [(0000, 1111)]
+        with patch('builtins.open', m_open):
+            hex_read = HexFile('')
+        self.assertRaises(HexError, hex_read.used_blocks, expected_value)
+
+    def test_block_data_error(self):
+        hex_file = io.BytesIO(b":03000000020008F3")
+        m_open = Mock(return_value=hex_file)
+        hex_file.name = ''
+        expected_value = [(-111, -222)]
+        with patch('builtins.open', m_open):
+            hex_read = HexFile('')
+        self.assertRaises(HexError, hex_read.used_blocks, expected_value)
+
+    def test_data_bytes_error(self):
+        hex_file = io.BytesIO(b":03000000")
+        m_open = Mock(return_value=hex_file)
+        hex_file.name = ''
+        with patch('builtins.open', m_open):
+            hex_read = HexFile('')
+        self.assertRaises(HexError, next, hex_read.data_bytes())
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ smashlib/test_micro.py
@@ -0,0 +1,106 @@
+import io
+import unittest
+from unittest.mock import Mock, patch
+from smashlib.micro import Micro
+from smashlib.micro import  HexDispData, HexBlankCheck, ProtoError
+from smashlib.micro import IspChecksumError, IspProgError, IspTimeoutError
+
+class HexTestCase(unittest.TestCase):
+
+    def test_dispdata(self):
+        hex_disp = HexDispData(0x20, 0x8)
+        expected_value = b":050000040020000800cf"
+        self.assertEqual(hex_disp.hex, expected_value)
+
+    def test_blank_check(self):
+        hex_blank = HexBlankCheck(0x20, 0x8)
+        expected_value = b":050000040020000801ce"
+        self.assertEqual(hex_blank.hex, expected_value)
+
+    def test_hex_dip(self):
+        self.assertRaisesRegex(ValueError, "data start address 0x54321 out of range",
+                               HexDispData, 0x54321, 0x4321)
+        self.assertRaisesRegex(ValueError, "data end address 0x43266 out of range",
+                               HexDispData, 0x5432, 0x43266)
+
+    def test_hex_dip_error(self):
+        self.assertRaisesRegex(ValueError, "check start address 0x54321 out of range",
+                               HexBlankCheck, 0x54321, 0x4321)
+        self.assertRaisesRegex(ValueError, "check end address 0x43266 out of range",
+                               HexBlankCheck, 0x5432, 0x43266)
+
+class MicroTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.serial = Mock()
+        self.micro_obj = Micro("P89V51RD2", 12, self.serial)
+
+    def file_obj(self):
+        hex_file = io.BytesIO(b":0300610002000397\n:0300610002000397")
+        m_open = Mock(return_value=hex_file)
+        return m_open
+
+    def test_sync_baudrate(self):
+        self.micro_obj.sync_baudrate()
+        self.assertEqual(self.micro_obj.serial.write.called, True)
+        self.assertEqual(self.micro_obj.serial.wait_for.called, True)
+        self.serial.wait_for.assert_called_with(b"U")
+
+    def test_sync_baudrate_error(self):
+        self.serial.wait_for.side_effect = IspTimeoutError('')
+        self.assertRaises(IspTimeoutError, self.micro_obj.sync_baudrate)
+
+    def test_program_file(self):
+        self.serial.wait_for.return_value = b"."
+        with patch('builtins.open', self.file_obj()):
+            self.micro_obj.prog_file("", Mock())
+        self.assertEqual(self.micro_obj.serial.write.called, True)
+        self.assertEqual(self.micro_obj.serial.wait_for.called, True)
+
+    def test_send_cmd_checksum(self):
+        self.serial.wait_for.return_value = b"X"
+        with patch('builtins.open', self.file_obj()):
+            self.assertRaises(IspChecksumError, self.micro_obj.prog_file, "")
+
+    def test_send_cmd_progem_error(self):
+        self.serial.wait_for.return_value = b"R"
+        hex_file = io.BytesIO(b":03000000020008")
+        m_open = Mock(return_value=hex_file)
+        with patch('builtins.open', m_open):
+            self.assertRaises(IspProgError, self.micro_obj.prog_file, "")
+
+    def test_send_cmd_progem_error_3(self):
+        self.serial.wait_for.return_value = b"P"
+        hex_file = io.BytesIO(b":03000000020008")
+        m_open = Mock(return_value=hex_file)
+        with patch('builtins.open', m_open):
+            self.assertRaises(IspProgError, self.micro_obj.prog_file, "")
+
+    def test__getitem__(self):
+        data = [b":", b"\r", b"0x0000=0E", b""]
+        self.micro_obj.serial.read_timeo.side_effect = data
+        self.assertEqual(self.micro_obj[0x0000], 14)
+        self.assertEqual(self.micro_obj.serial.write.called, True)
+
+    def test__update_cache_with_line_error(self):
+        data = [b'0x0000', b''] * 8
+        self.micro_obj.serial.read_timeo.side_effect = data
+        self.assertRaises(ProtoError, lambda: self.micro_obj[0xffff])
+
+    def test__update_cache_with_line_addr_error(self):
+        data = [b'0xghgj=pp', b''] * 8
+        self.micro_obj.serial.read_timeo.side_effect = data
+        self.assertRaises(ProtoError, lambda: self.micro_obj[0x0000])
+
+    def test__update_cache_with_line_data_error(self):
+        data = [b'0xghgj=7', b''] * 8
+        self.micro_obj.serial.read_timeo.side_effect = data
+        self.assertRaises(ProtoError, lambda: self.micro_obj[0x0000])
+
+    def test_getitem_error(self):
+        data = [b'0x0001=0E', b''] * 8
+        self.micro_obj.serial.read_timeo.side_effect = data
+        self.assertRaises(ProtoError, lambda: self.micro_obj[0x0000])
+
+if __name__ == '__main__':
+    unittest.main()
